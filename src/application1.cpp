31d5dad538b4891056ecc31f68af7b705e6a701d
AlexWeeeng22 <wpc1021838281@163.com>
2024-08-06 14:55:09 -0400

    The first commit from Alex, application1 on opengl, gave fun sir!

---
+++ src/application1.cpp
@@ -0,0 +1,54 @@
+//#include <iostream>
+//
+//int main() {
+//	std::cout << "Hello Motherfuckers" << std::endl;
+//	std::cin.get();
+//}
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Show GL version we are using */
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
