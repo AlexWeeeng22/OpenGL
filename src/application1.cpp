d590f895d50946c4a8b8d047125192e357cf2f3b
AlexWeeeng22 <wpc1021838281@163.com>
2024-08-07 14:23:30 -0400

    commit test 20240807 14:23 --Alex

--- src/application1.cpp
+++ src/application1.cpp
@@ -6,13 +6,16 @@
 //}
 
 #include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 
 int main(void)
 {
     GLFWwindow* window;
     
     std::cout << "Hello Motherfuckers, im your dear father~" << std::endl;
+
 
 
     /* Initialize the library */
@@ -30,8 +33,25 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Check if GLEW is ready */
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "error" << std::endl;
+    }
+
+
+    unsigned int a;
+    glGenBuffers(1, &a);
+
+
     /* Show GL version we are using */
     std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f
+    };
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -39,11 +59,11 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        //glBegin(GL_TRIANGLES);
+        //glVertex2f(-0.5f, -0.5f);
+        //glVertex2f(0.0f, 0.5f);
+        //glVertex2f(0.5f, -0.5f);
+        //glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
